9645ccd34dc2732ad71fcbe313fcf233412ab25f
Thomas Pratama <pratama.thomas@gmail.com>
2024-11-29 20:34:56 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+      },
+    ],
+  },
+};
