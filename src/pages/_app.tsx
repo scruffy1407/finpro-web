01d6bd64704e4be1d1d55cfbfea498f8bc56165a
digiT000 <fareldeksano000@gmail.com>
2024-12-02 20:48:29 +0700

    Complete Slicing Login Page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster
+        closeButton={true}
+        richColors={true}
+        theme={`light`}
+        position={`top-right`}
+      />
+    </>
+  );
 }
