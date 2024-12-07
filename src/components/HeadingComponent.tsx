c699b57c2b3194526275e21034b3d7e26b58209e
Farel Darari Deksano <65439249+digiT000@users.noreply.github.com>
2024-12-07 10:27:30 +0700

    styles : fix style and responsive landing page

--- src/components/HeadingComponent.tsx
+++ src/components/HeadingComponent.tsx
@@ -3,26 +3,21 @@ import ButtonComponent from "./ButtonComponent";
 import { HeadingProps } from "@/utils/interface";
 
 function HeadingComponent({ heading, paragraph }: HeadingProps) {
-	return (
-		<div className="max-w-screen-xl mx-auto px-4 md:px-0 ">
-			<div className="flex justify-between">
-				<div className="flex flex-col gap-5">
-					<div>
-						<h2 className="text-2xl font-semibold text-neutral-900">
-							{heading}
-						</h2>
-					</div>
-					<div>
-						<p>{paragraph}</p>
-					</div>
-				</div>
+  return (
+    <div className="max-w-screen-xl mx-auto px-4 md:px-0 ">
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-5">
+          <h2 className="text-2xl font-semibold text-neutral-900">{heading}</h2>
 
-				<div className="hidden md:flex justify-center items-center">
-					<ButtonComponent container="Explore More" type="ButtonBorder" />
-				</div>
-			</div>
-		</div>
-	);
+          <p className={`text-neutral-600`}>{paragraph}</p>
+        </div>
+
+        <div className="hidden md:flex justify-center items-center">
+          <ButtonComponent container="Explore More" type="ButtonBorder" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HeadingComponent;
